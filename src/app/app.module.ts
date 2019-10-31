fffc05df6c36fd25b94bf0f60793eec625aaf539
ArashiNoKishi <arashinokishi@gmail.com>
2019-10-31 09:37:05 -0500

    3 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,13 @@ import { MessagesComponent } from './messages/messages.component';
 import { MessageService } from './message.service';
 import { AppRoutingModule }     from './app-routing.module';
 import { MassSimulationComponent } from './mass-simulation/mass-simulation.component';
+import { IchorComponent } from './ichor/ichor.component';
+import { IchorService } from './ichor.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, AppRoutingModule ],
-  declarations: [ AppComponent, WeaponComponent, MessagesComponent, MassSimulationComponent ],
+  declarations: [ AppComponent, WeaponComponent, MessagesComponent, MassSimulationComponent, IchorComponent ],
   bootstrap:    [ AppComponent ],
-  providers: [EnhancementService, MessageService]
+  providers: [EnhancementService, MessageService, IchorService]
 })
 export class AppModule { }
